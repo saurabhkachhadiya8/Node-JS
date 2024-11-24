3ff7c2b67f02eacc9d0ebd48ae390621352b1d69
saurabhkachhadiya8 <saurabhkachhadiya890@gmail.com>
2024-11-25 02:14:02 +0545

    Node-JS

--- 05 crud/index.js
+++ 05 crud/index.js
@@ -1,18 +1,58 @@
 import express from 'express';
 const port = 8080;
 const app = express();
-const record = [
-    { id: 1, name: 'raj', phone: 123 },
-    { id: 1, name: 'raj', phone: 123 },
-    { id: 1, name: 'raj', phone: 123 },
-    { id: 1, name: 'raj', phone: 123 },
-];
+let record = [];
+let editUser;
 
 app.set('view engine', 'ejs');
+app.use(express.urlencoded());
+
 app.get('/', (req, res) => {
     return res.render('index', {
-        data: record
+        data: record,
+        editUser : null
     });
+});
+
+app.post('/adduser',(req,res)=>{
+    const {username,userphone} = req.body;
+    let obj = {
+        id : Date.now(),
+        name : username,
+        phone : userphone
+    }
+    record.push(obj);
+    console.log("User Add Successfully.");
+    return res.redirect('/');
+});
+app.get('/deleteuser',(req,res)=>{
+    let id = req.query.deleteId;
+    let deleteuser = record.filter(val => val.id != id);
+    record = deleteuser;
+    console.log("User Delete Successfully.");
+    return res.redirect('/');
+});
+app.get('/edituser',(req,res)=>{
+    let id = req.query.editId;
+    let edituser = record.find(val => val.id == id);
+    editUser = edituser;
+    return res.render('index',{
+        data : record,
+        editUser
+    });
+});
+app.post('/updateuser',(req,res)=>{
+    const{updateId,username,userphone} = req.body;
+    let updateuser = record.map(val => {
+        if(val.id == updateId){
+            val.name = username;
+            val.phone = userphone;
+        }
+        return val;
+    });
+    record = updateuser;
+    console.log("User Update Successfully.");
+    return res.redirect('/');
 });
 
 app.listen(port, (err) => {
