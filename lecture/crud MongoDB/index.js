d09f12e19be226594750d124b06d40f45ea1393c
patel-reema <reemapatel8189@gmail.com>
2024-12-11 17:33:09 +0530

    Nodfe-JS

--- lecture/crud MongoDB/index.js
+++ lecture/crud MongoDB/index.js
@@ -2,7 +2,8 @@ const express = require('express');
 const port = 8080;
 const app = express();
 
-const db = require('./config/db')
+const db = require('./config/db');
+const User = require('./models/UserModel');
 
 app.set('view engine','ejs');
 
@@ -11,6 +12,19 @@ app.get('/',(req,res)=>{
 });
 app.get('/add',(req,res)=>{
     return res.render('add');
+});
+app.post('/adduser',(req,res)=>{
+    const {username,useremail,userpass} = req.body;
+    User.create({
+        name : username,
+        email : useremail,
+        password : userpass
+    }).then((data)=>{
+        console.log(data);
+    }).catch((err)=>{
+        console.log(err);
+        return false;
+    });
 });
 
 app.listen(port,(err)=>{
