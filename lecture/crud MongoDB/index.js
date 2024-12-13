658e07b4182e8f6745668533dc47c3b79fff58f5
saurabhkachhadiya8 <saurabhkachhadiya890@gmail.com>
2024-12-13 13:05:08 +0530

    Node-JS

--- lecture/crud MongoDB/index.js
+++ lecture/crud MongoDB/index.js
@@ -3,32 +3,36 @@ const port = 8080;
 const app = express();
 
 const db = require('./config/db');
+
+app.set('view engine', 'ejs');
+
 const User = require('./models/UserModel');
 
-app.set('view engine','ejs');
+app.use(express.urlencoded());
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     return res.render('view');
 });
-app.get('/add',(req,res)=>{
+app.get('/add', (req, res) => {
     return res.render('add');
 });
-app.post('/adduser',(req,res)=>{
-    const {username,useremail,userpass} = req.body;
+app.post('/adduser', (req, res) => {
+    const { username, useremail, userpass } = req.body;
     User.create({
-        name : username,
-        email : useremail,
-        password : userpass
-    }).then((data)=>{
-        console.log(data);
-    }).catch((err)=>{
+        name: username,
+        email: useremail,
+        password: userpass
+    }).then((data) => {
+        console.log(`record add.`);
+        return res.redirect('/');
+    }).catch((err) => {
         console.log(err);
         return false;
     });
 });
 
-app.listen(port,(err)=>{
-    if(err){
+app.listen(port, (err) => {
+    if (err) {
         console.log(err);
         return false;
     }
