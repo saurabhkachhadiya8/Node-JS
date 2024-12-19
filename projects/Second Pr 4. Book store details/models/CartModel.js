7b3df4e3a1b3ff46f7a10c9b8cf9216bdb44af85
saurabhkachhadiya8 <saurabhkachhadiya890@gmail.com>
2024-12-19 14:55:09 +0530

    Node-JS

---
+++ projects/Second Pr 4. Book store details/models/CartModel.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const cartSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    pages: {
+        type: Number,
+        required: true
+    },
+    author: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    }
+});
+const cart = mongoose.model('cart',cartSchema);
+module.exports = cart;
