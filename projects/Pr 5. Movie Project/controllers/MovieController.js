cb4008b340474e136f08d4243680f2f8760d2f51
saurabhkachhadiya8 <saurabhkachhadiya890@gmail.com>
2024-12-24 14:55:42 +0530

    Node-JS

--- projects/Pr 5. Movie Project/controllers/MovieController.js
+++ projects/Pr 5. Movie Project/controllers/MovieController.js
@@ -1,8 +1,12 @@
 const movieModel = require('../models/MovieModel');
+const movieApi = require('../config/movieApi.json');
 
 const index = async(req,res)=>{
     try{
-        return res.render('index');
+        const movies = await movieApi.movies;
+        return res.render('index',{
+            movies
+        });
     }catch(err){
         console.log(err);
         return false;
