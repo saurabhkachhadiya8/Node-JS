041b96d5edf28b3ea718894b96dabc83156491a0
Abhaykahar <ak2415112@gmail.com>
2025-02-12 18:09:48 +0530

    Node-JS

--- Practical Exam/routes/indexRoute.js
+++ Practical Exam/routes/indexRoute.js
@@ -3,6 +3,6 @@ const routes = express();
 const passport = require('passport');
 
 routes.use('/', require('./authRoute'));
-routes.use('/dashboard'/*, passport.checkUser*/, require('./dashboardRoute'));
+routes.use('/dashboard', passport.checkUser, require('./dashboardRoute'));
 
 module.exports = routes;
